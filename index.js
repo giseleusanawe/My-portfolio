7ec62a8059721538d097983b09d4ad161204d8ea
Gisele <giselleusanase@gmail.com>
2023-04-26 14:32:10 +0200

    my portfolio

---
+++ index.js
@@ -0,0 +1,150 @@
+// let num =[2,1,3,4,5,1,3,4,1,6];
+// let result=num.lastIndexOf(1);
+// let result1=num.lastIndexOf(1,4)
+
+// console.log(result);
+// console.log(result1);
+
+
+// let fruit = ['mango','apple', 'avocado', 'pinapple'];
+// let sorted fruit
+
+
+// function iseven(element){
+//     return element%2==0
+// }
+
+// let numbers = [1,3,4,6,8,9];
+// let removedNumber =numbers.splice(2,0,10);
+// console.log(removedNumber);
+// console.log(numbers);
+
+
+// let numbers = [1,3,4,5,6,8,9];
+// let removedNumber =numbers.slice(3);
+// console.log(removedNumber);
+// // console.log(numbers);
+
+
+
+// let numberSub = [100,50,15];
+
+// function substract(out,num){
+//     return out - num;
+// }
+// console.log(numberSub.reduceRight(substract));
+
+
+// class Student{
+//     learn(){
+//         console.log("Our student is in classroom");
+//     }
+//     work(){
+//       console.log("Students are doing their assignment");
+//     }
+// }
+// var student1 = new Student();//object
+// console.log(student1.work());
+
+
+
+// class StudentDetail{
+//     constructor(name,Dob, location,parentsname){
+//         this.name=name;
+//         this.Dob=Dob;
+//         this.location=location;
+//         this.parentsname=parentsname;
+
+//     }
+// }
+// var studentDetail1 = new studentDetail('Shea','15thOct','Kicukiro','shearunna');
+// console.log(studentDetail1.name);
+// console.log(studentDetail1.Dob);
+// console.log(studentDetail1.location);
+// console.log(studentDetail1.parentsname);
+
+
+
+
+
+
+
+
+// class ChildDetail{
+//     constructor(name,Dob,location,parentsname){
+//         this.name=name;
+//         this.Dob=Dob;
+//         this.location=location;
+//         this.parentsname=parentsname;
+
+//     }
+// }
+// var childDetail1 = new ChildDetail("Shea","15thOct","Kicukiro","shearunna");
+// console.log(childDetail1.name + " "+ childDetail1.Dob +" "+ childDetail1.location+" "+ childDetail1.location+ " " + childDetail1.parentsname);
+// // console.log(childDetail1.Dob);
+// // console.log(childDetail1.location);
+// // console.log(childDetail1.parentsname);
+
+
+// var childDetail2 = new ChildDetail("Cathy", "24thNov", "Kanombe","Nkusi");
+// console.log(childDetail2.name+" " + childDetail2.Dob+ " " + childDetail2.location+" " + childDetail2.location+" " + childDetail2.parentsname);
+
+// // console.log(childDetail2.name);
+// // console.log(childDetail2.Dob);
+// // console.log(childDetail2.location);
+// // console.log(childDetail2.parentsname);
+
+// var childDetail3 = new ChildDetail("Dez","16thJune","Niboye","Vugu");
+// console.log(childDetail3.name +" "+ childDetail3.Dob+ " " + childDetail3.location+ " " + childDetail3.parentsname);
+// // console.log(childDetail3.name);
+// // console.log(childDetail3.Dob);
+// // console.log(childDetail3.location);
+// // console.log(childDetail3.parentsname);
+
+// var childDetail4 = new ChildDetail("Cb","13thMay","Miami","Brown");
+// console.log(childDetail4.name+ " " + childDetail4.Dob+ " " + childDetail4.location+ " " + childDetail4.parentsname);
+// // console.log(childDetail.name);
+// // console.log(childDetail.Dob);
+// // console.log(childDetail.location);
+// // console.log(childDetail.parentsname);
+
+// var childDetail5 = new ChildDetail("Carol", "24thNov", "Kabuga", "Nkusi");
+// console.log(childDetail5.name + childDetail5.Dob + childDetail5.location + childDetail5.parentsname);
+// // console.log(childDetail5.name);
+// // console.log(childDetail5.Dob);
+// // console.log(childDetail5.location);
+// // console.log(childDetail5.parentsname);
+
+// var childDetail6 = new ChildDetail("Hailey","12thNov","Lasvegas","Eminem");
+// console.log(childDetail6.name + " "+ childDetail6.Dob+ " " + childDetail6.location+ " " + childDetail6.parentsname);
+// // console.log(childDetail6.name);
+// // console.log(childDetail6.Dob);
+// // console.log(childDetail6.location);
+// // console.log(childDetail6.parentsname);
+
+
+// var childDetail7 = new ChildDetail("Brown","18thFeb", "Nyanza", "Gigi");
+// console.log(childDetail7.name+ " " + childDetail7.Dob+ " " + childDetail7.location+ " " + childDetail7.parentsname);
+
+// var childDetail8 = new ChildDetail("Berry", "12thMay","Nyamirambo", "Hadidi");
+// console.log(childDetail8.name+ " " + childDetail8.Dob+ " " + childDetail8.location+ " " + childDetail8.parentsname);
+
+// var childDetail9 = new ChildDetail("Miah", "14thOct","Muyange", "Isengwe");
+// console.log(childDetail9.name+ " " + childDetail9.Dob+ " " + childDetail9.location+ " " + childDetail9.parentsname);
+
+// var childDetail10 = new ChildDetail("Harry","24thMar", "England", "Diana" );
+// console.log(childDetail10.name+ " " + childDetail10.Dob+ " " + childDetail10.location+ " " + childDetail10.parentsname);
+
+
+
+// var myString = "A front-End Developer is someone who creates websites and web application"
+
+
+// var sliceMyString = myString.slice(5,9);
+// console.log(sliceMyString)
+
+// const string = "now we are learning javascript";
+// const reverseString = string.split("").reverse().join("");
+// console.log(reverseString);
+
+
